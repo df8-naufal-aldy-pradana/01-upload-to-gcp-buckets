9dd98d144effc7fd32b7a944c5e5528481a33086
blitzkz23 <blitzaldykazu@gmail.com>
2022-11-29 09:07:59 +0700

    feat: modify upload function

--- gcp-upload.py
+++ gcp-upload.py
@@ -5,17 +5,18 @@ import configparser
 config = configparser.ConfigParser()
 config.read_file(open('gcp.config'))
 
+# Replace with your own credentials
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'df8-naufal-key.json'
 
-# Initiates Variable
+# Initiates Variable from config file
 project_id = config['GCP']['PROJECT_ID']
 bucket_name = config['GCP']['BUCKET_NAME']
 dest_name = config['GCP']['DESTINATION_NAME']
 local_file_path = config['GCP']['LOCAL_FILE_PATH']
 
 client = storage.Client(project_id)
-print(local_file_path)
 
+# Define function to upload file to GCP
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
     try:
         bucket = client.get_bucket(bucket_name)
@@ -24,6 +25,8 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
         print('File {} uploaded to {}.'.format(source_file_name, destination_blob_name))
     except Exception as e:
         print(e)
+        print('File {} failed to upload to {}.'.format(source_file_name, destination_blob_name))
 
-upload_blob(bucket_name, local_file_path, dest_name)
-
+# Call function to upload file to GCP
+for file in os.listdir(local_file_path):
+    upload_blob(bucket_name, local_file_path + file, dest_name + file)
