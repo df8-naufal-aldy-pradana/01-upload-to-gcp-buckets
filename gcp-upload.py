29b79409d4133cde467e1cf8652d1a5ffc8fa3da
blitzkz23 <blitzaldykazu@gmail.com>
2022-11-30 08:25:25 +0700

    feat: add new function to upload file from internet

--- gcp-upload.py
+++ gcp-upload.py
@@ -1,6 +1,7 @@
 import os
 from google.cloud import storage
 import configparser
+import urllib.request
 
 config = configparser.ConfigParser()
 config.read_file(open('gcp.config'))
@@ -14,10 +15,14 @@ bucket_name = config['GCP']['BUCKET_NAME']
 dest_name = config['GCP']['DESTINATION_NAME']
 local_file_path = config['GCP']['LOCAL_FILE_PATH']
 
+# File from internet
+random_internet_img = 'https://images.unsplash.com/photo-1669639070423-462483f0211b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+file = urllib.request.urlopen(random_internet_img)
+
 client = storage.Client(project_id)
 
-# Define function to upload file to GCP
-def upload_blob(bucket_name, source_file_name, destination_blob_name):
+# Define function to upload file to GCP from local assets
+def upload_blob_filename(bucket_name, source_file_name, destination_blob_name):
     try:
         bucket = client.get_bucket(bucket_name)
         blob = bucket.blob(destination_blob_name)
@@ -27,6 +32,20 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
         print(e)
         print('File {} failed to upload to {}.'.format(source_file_name, destination_blob_name))
 
-# Call function to upload file to GCP
-for file in os.listdir(local_file_path):
-    upload_blob(bucket_name, local_file_path + file, dest_name + file)
+# Call function to upload file from local assets to GCP
+for img in os.listdir(local_file_path):
+    upload_blob_filename(bucket_name, local_file_path + img, dest_name + img)
+
+# Define function to upload file to GCP from internet
+def upload_blob_string(bucket_name, source_file, destination_blob_name):
+    try:
+        bucket = client.get_bucket(bucket_name)
+        blob = bucket.blob(destination_blob_name)
+        blob.upload_from_string(source_file, content_type='image/jpeg')
+        print('File {} uploaded to {}.'.format("Random Internet Image", destination_blob_name))
+    except Exception as e:
+        print(e)
+        print('File {} failed to upload to {}.'.format(source_file, destination_blob_name))
+
+# Call function to upload file from internet to GCP
+upload_blob_string(bucket_name, file.read(), dest_name + 'random_internet_img.jpg')
